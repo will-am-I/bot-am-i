0839a7c3bd3b1e805fb3a8553cb63c5a6d7e3031
Will Reaves <wjreaves93@yahoo.com>
2019-06-10 16:20:28 -0400

    huh?

--- bot-am-i.py
+++ bot-am-i.py
@@ -31,4 +31,4 @@ for filename in os.listdir('./cogs'):
    if filename.endswith('.py'):
       client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(config['discord_id'])
+client.run(config['discord_token'])
